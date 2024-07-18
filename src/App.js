21cad2a6182452320ec347f601ca73629100a855
Maxim Valevatyi <Stayspaceflux@gmail.com>
2024-07-18 17:45:13 +0300

    f

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './About';
+import Point from './Point';
+import FirstMain from './FirstMain';
+import LastMain from './LastMain';
+import LastHeader from './components/LastHeader';
+import A from './components/A';
+
+
+const ZNZN = () => {
+
+
+  return (  
+<div className="ZNZN">
+  <Header />
+  <LastHeader />      
+  <Point />
+ <FirstMain />
+ <LastMain />
+  <A />
+  <About />
+  <Footer />
+</div>
+);
+}
+export default ZNZN;
